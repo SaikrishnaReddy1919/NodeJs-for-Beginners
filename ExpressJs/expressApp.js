7dc07edb36c2e491fdfeb5349fe0d3122aa10177
Saikrishna Reddy <krishnasvr9393@gmail.com>
2020-10-11 19:56:28 +0530

    Added 'ExpressJS' app [Blog], implemented 'EJS' [ Embeded javascript templates] - server side rendering.

--- ExpressJs/expressApp.js
+++ ExpressJs/expressApp.js
@@ -2,27 +2,34 @@ const express = require("express");
 
 const app = express();
 
+//register view engine
+
+app.set('view engine', 'ejs')
+
 app.get("/", (req, res) => {
-  res.sendFile("./routes/home.html", { root: __dirname });
+  const blogs = [
+    {title : 'Intro to Ethereum', snippet: 'One stop guide to Ethereum'},
+    {title : 'Intro to Ethereum', snippet: 'One stop guide to Ethereum'},
+    {title : 'Intro to Ethereum', snippet: 'One stop guide to Ethereum'},
+  ]
+  res.render('index', {title : 'Home', blogs})
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./routes/about.html", { root: __dirname });
+  res.render('about', {title : 'about'})
 });
 
-//redirect
-app.get("/about-us", (req, res) => {
-    res.redirect('/about')
+app.get('/blogs/create', (req, res) => {
+  res.render('create', {title : 'Create a new Blog'})
 })
 
 //404 -> If any route above doesnt match with the url requested by the user then following method will be fired. **POSITION OF BELOW CODE IS IMPORTANT. SHOULD BE AT LAST. WE ARE TELLING TO EXPRESS THAT -> IF NOTHING MATCHES EXECUTE BELOW.**
 app.use((req, res) => {
-    res.status(404).sendFile("./routes/404.html", { root: __dirname })
+    res.status(404).render('404', {title : '404'})
 })
 
 app.listen(3000, () => {
-  const dir = __dirname;
-  console.log(dir, "Listening on port 8000");
+  console.log("Listening on port 8000");
 });
 
 //compare this code with the file 'serverAndClient/server.js'. Express make things a lot easier.
