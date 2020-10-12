8fa18d5ce7b0177d80e158672a86bd1b19e5e9c7
Saikrishna Reddy <krishnasvr9393@gmail.com>
2020-10-13 01:59:48 +0530

    Implemented MVC architecture, more modularity.

--- ExpressJs/expressApp.js
+++ ExpressJs/expressApp.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const morgon = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
-const { render } = require("ejs");
+// const { render } = require("ejs");
+const blogRoutes = require("./routes/blogRoutes");
 
 const app = express();
 
@@ -20,64 +20,8 @@ mongoose
   })
   .catch((err) => console.log(err));
 
-//add a new blog
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "New Blog",
-    snippet: "About my new blog",
-    body: "More about my new blog",
-  });
-  const blog2 = new Blog({
-    title: "New Blog 2",
-    snippet: "About my new blog",
-    body: "More about my new blog",
-  });
-
-  blog
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => console.log(err));
-
-  blog2
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => console.log(err));
-});
-
-//get all blogs
-app.get("/all-blogs", (req, res) => {
-  Blog.find()
-    .then((result) => res.send(result))
-    .catch((err) => console.log(err));
-});
-
-//get single blog
-app.get("/single-blog", (req, res) => {
-  Blog.findById("5f83d21c67e66e7ac3972ffe")
-    .then((result) => res.send(result))
-    .catch((err) => console.log(err));
-});
-
-//------------------------------
-
 //register view engine
 app.set("view engine", "ejs");
-
-//middleware
-// app.use((req, res, next) => {
-//   console.log('New request was made')
-//   console.log("host : ", req.hostname)
-//   console.log("path : ", req.path)
-//   console.log('method : ', req.method)
-//   next()
-// })
-
-//using morgon
-app.use(morgon("dev"));
 
 //middleware & static files -> images, .css etc...
 app.use(express.static("views/partials/public"));
@@ -86,59 +30,21 @@ app.use(express.static("views/partials/public"));
 //All the data will be added to request object. -> req.body
 app.use(express.urlencoded({ extended: true }));
 
+//using morgon
+app.use(morgon("dev"));
+
 //routes
 app.get("/", (req, res) => {
   res.redirect("/blogs");
 });
 
+
+//blogroutes
+app.use('/blogs', blogRoutes)
+
 app.get("/about", (req, res) => {
   res.render("about", { title: "about" });
 });
-
-//blog-routes
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("index", { title: "All Blogs", blogs: result });
-    })
-    .catch((err) => console.log(err));
-});
-
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get("/blogs/:id", (req, res) => {
-  Blog.findById(req.params.id)
-    .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.delete("/blogs/:id", (req, res) => {
-  Blog.findByIdAndDelete(req.params.id)
-    .then((result) => {
-      res.json({
-        redirect : '/blogs'
-      })
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "Create a new Blog" });
-});
-
 //404 -> If any route above doesnt match with the url requested by the user then following method will be fired. **POSITION OF BELOW CODE IS IMPORTANT. SHOULD BE AT LAST. WE ARE TELLING TO EXPRESS THAT -> IF NOTHING MATCHES EXECUTE BELOW.**
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
