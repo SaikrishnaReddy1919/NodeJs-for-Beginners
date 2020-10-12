255fa9b72537e68ddae96daa3550db8affde5c1c
Saikrishna Reddy <krishnasvr9393@gmail.com>
2020-10-12 14:41:43 +0530

    Added Mongodb as a DB. Getting/posting data from/to the db

--- ExpressJs/expressApp.js
+++ ExpressJs/expressApp.js
@@ -1,11 +1,66 @@
 const express = require("express");
-const morgon = require('morgan')
+const morgon = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 const app = express();
 
-//register view engine
+//connect to db
+const DB_URI =
+  "mongodb+srv://---------@@ecommerce.wxheo.mongodb.net/Node-tuts";
 
-app.set('view engine', 'ejs')
+mongoose
+  .connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Connected to DB");
+      console.log("Listening on port 8000");
+    });
+  })
+  .catch((err) => console.log(err));
+
+  //add a new blog
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'New Blog',
+    snippet: 'About my new blog',
+    body:'More about my new blog'
+  })
+  const blog2 = new Blog({
+    title: 'New Blog 2',
+    snippet: 'About my new blog',
+    body:'More about my new blog'
+  })
+
+  blog.save().then((result) => {
+    res.send(result)
+  }).catch((err) => console.log(err))
+
+  blog2.save().then((result) => {
+    res.send(result)
+  }).catch((err) => console.log(err))
+})
+
+//get all blogs
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then((result) => res.send(result))
+  .catch((err) => console.log(err))
+})
+
+//get single blog
+app.get('/single-blog', (req, res) => {
+  Blog.findById("5f83d21c67e66e7ac3972ffe")
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err))
+})
+
+
+
+//------------------------------
+
+//register view engine
+app.set("view engine", "ejs");
 
 //middleware
 // app.use((req, res, next) => {
@@ -17,37 +72,38 @@ app.set('view engine', 'ejs')
 // })
 
 //using morgon
-app.use(morgon('dev'))
+app.use(morgon("dev"));
 
 //middleware & static files -> images, .css etc...
-app.use(express.static('views/partials/public'))
+app.use(express.static("views/partials/public"));
 //provide the full path of a folder you want to make public.
 
 
+//routes
 app.get("/", (req, res) => {
-  const blogs = [
-    {title : 'Intro to Ethereum', snippet: 'One stop guide to Ethereum'},
-    {title : 'Intro to Ethereum', snippet: 'One stop guide to Ethereum'},
-    {title : 'Intro to Ethereum', snippet: 'One stop guide to Ethereum'},
-  ]
-  res.render('index', {title : 'Home', blogs})
+  res.redirect('/blogs')
 });
 
 app.get("/about", (req, res) => {
-  res.render('about', {title : 'about'})
+  res.render("about", { title: "about" });
 });
 
-app.get('/blogs/create', (req, res) => {
-  res.render('create', {title : 'Create a new Blog'})
+//blog-routes
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt : -1 })
+    .then((result) => {
+    res.render('index', { title : 'All Blogs', blogs : result })
+    })
+  .catch((err) => console.log(err))
 })
+
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
+});
 
 //404 -> If any route above doesnt match with the url requested by the user then following method will be fired. **POSITION OF BELOW CODE IS IMPORTANT. SHOULD BE AT LAST. WE ARE TELLING TO EXPRESS THAT -> IF NOTHING MATCHES EXECUTE BELOW.**
 app.use((req, res) => {
-    res.status(404).render('404', {title : '404'})
-})
-
-app.listen(3000, () => {
-  console.log("Listening on port 8000");
+  res.status(404).render("404", { title: "404" });
 });
 
 //compare this code with the file 'serverAndClient/server.js'. Express make things a lot easier.
